e3fb69a6a146f21a22abe56773537d7f9b5f8e5f
Gerard Pairo <gerardpairo@hotmail.com>
2023-04-24 16:01:17 +0200

    setting upp

---
+++ condicionales/app.js
@@ -0,0 +1,27 @@
+// EJERCICIO 1
+//////////////
+// let numInput = prompt("Escribe 2 números separados por ',' y haremos los cálculos").split(',');
+// let number1 = parseFloat(numInput[0]);
+// let number2 = parseFloat(numInput [1]);
+
+// let calculate = (a, b) => {
+//     let sum = a + b;
+//     let substraction = a - b;
+//     let product = a * b;
+//     let average = (a + b) / numInput.length;  
+    
+//     console.log(`The sum is ${sum}. The substraction is ${substraction}.
+//     The product is ${product}. The average is ${average}`);
+// }
+
+// console.log(calculate(number1, number2));
+
+// if (number1 > number2) {
+//     console.log(` The biggest number is ${number1} and the lowest is ${number2}`)
+// } else if (number1 < number2) {
+//     console.log(` The biggest number is ${number2} and the lowest is ${number1}`)
+
+// }
+
+// EJERCICIO 2
+//////////////
